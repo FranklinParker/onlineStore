d5a42eca649aae59ae6be8af4048db41903e434c
FranklinParker <fparker@deliveryman.com>
2021-05-07 16:20:45 -0400

    svae

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,30 @@ import { RouterModule, Routes } from '@angular/router';
 import {StoreComponent} from './store/store.component';
 import {CartDetailsComponent} from './cart-details/cart-details.component';
 import {CheckoutComponent} from './checkout/checkout.component';
+import {StoreGuard} from './store/store.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: StoreComponent,
+    canActivate: [StoreGuard]
+
   },
   {
     path: 'cart',
-    component: CartDetailsComponent
+    component: CartDetailsComponent,
+    canActivate: [StoreGuard]
   },
   {
     path: 'checkout',
-    component: CheckoutComponent
-  }
+    component: CheckoutComponent,
+    canActivate: [StoreGuard]
+  },
+  {
+    path: '**',
+    component: StoreComponent
+  },
+
 ];
 
 @NgModule({
---
+++ src/app/store/store.guard.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {StoreComponent} from './store.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoreGuard implements CanActivate {
+  firstNav = true;
+
+  constructor(private router: Router) {
+
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.firstNav) {
+      this.firstNav = false;
+      if (route.component !== StoreComponent) {
+        this.router.navigate(['']);
+        return false;
+      }
+    }
+    return true;
+  }
+
+
+}
