5d52f78defafd92e470522cb0b1ff5ba774366fa
FranklinParker <fparker@deliveryman.com>
2021-05-07 08:45:43 -0400

    svae

--- src/app/models/cart.model.ts
+++ src/app/models/cart.model.ts
@@ -5,13 +5,27 @@ import {ProductModel} from '../../model/product.model';
 @Injectable()
 export class CartModel{
   private cartLines: CartLine[] = [];
-  updateCartLine(product: ProductModel, quantity: number ): void {
+  addToCart(product: ProductModel, quantity: number = 1 ): void {
     const cartLine =  this.cartLines
       .find((item) => item.product.id === product.id);
     if (cartLine){
-      cartLine.quantity = quantity;
+      cartLine.quantity += quantity;
     } else {
       this.cartLines.push(new CartLine(product, quantity));
+    }
+  }
+
+  updateQuantity(product: ProductModel, quantity: number): void {
+    const cartLine = this.cartLines
+      .find((item) => item.product.id === product.id);
+    if ( cartLine){
+      cartLine.quantity = quantity;
+    }
+  }
+  remove(product: ProductModel): void{
+    const idx = this.cartLines.findIndex(item=> item.product.id === product.id);
+    if (idx !== -1){
+      this.cartLines.splice(idx, 1);
     }
   }
 }
