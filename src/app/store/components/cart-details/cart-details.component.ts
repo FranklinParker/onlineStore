89b1e935ffb5e053d71bb68c85ae7541aa17a545
FranklinParker <fparker@deliveryman.com>
2021-05-08 10:29:49 -0400

    save

--- src/app/store/components/cart-details/cart-details.component.ts
+++ src/app/store/components/cart-details/cart-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CartModel} from '../../../models/cart.model';
 
 @Component({
   selector: 'app-cart-details',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private cartModel: CartModel) { }
 
   ngOnInit(): void {
   }
