4837dfc8706347cb1fd39e74b8cea9a5800e038a
FranklinParker <fparker@deliveryman.com>
2021-05-10 13:55:44 -0400

    save

--- src/model/model.module.ts
+++ src/model/model.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {ProductRepository} from './product.repository';
 import {StaticDataSource} from './staticDataSource';
 import {CartModel} from '../app/models/cart.model';
+import {OrderModel} from '../app/models/Order.model';
+import {OrderRepository} from './order.repository';
 
 @NgModule({
-  providers: [ProductRepository, StaticDataSource, CartModel]
+  providers: [ProductRepository, StaticDataSource, CartModel, OrderModel, OrderRepository]
 })
 export class ModelModule{
 }
