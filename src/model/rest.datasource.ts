bd6c7686da162d90701393a19ef79210d08fd293
FranklinParker <fparker@deliveryman.com>
2021-05-10 19:01:18 -0400

    svae

--- src/model/rest.datasource.ts
+++ src/model/rest.datasource.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {ProductModel} from './product.model';
+import {OrderModel} from '../app/models/Order.model';
 
 
 const PORT = 3500;
@@ -18,5 +19,9 @@ export class RestDatasource{
 
   getProduct(): Observable<ProductModel[]>{
     return this.httpClient.get<ProductModel[]>(this.baseUrl + 'products');
+  }
+
+  saveOrder(order: OrderModel): Observable<OrderModel>{
+    return this.httpClient.post<OrderModel>(this.baseUrl + 'orders', order);
   }
 }
