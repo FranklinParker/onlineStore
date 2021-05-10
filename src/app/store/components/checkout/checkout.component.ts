456d89791b70bf35bfdfb7089edeb24de48acac7
FranklinParker <fparker@deliveryman.com>
2021-05-10 14:36:51 -0400

    save

--- src/app/store/components/checkout/checkout.component.ts
+++ src/app/store/components/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {OrderModel} from '../../../models/Order.model';
+import {OrderRepository} from '../../../../model/order.repository';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
+  submitted = false;
+  orderSent = false;
 
-  constructor() { }
+  constructor(public order: OrderModel,
+              public orderRepository: OrderRepository) {
+  }
 
   ngOnInit(): void {
   }
 
+
+  save(form: NgForm): any {
+    if(form.valid){
+      this.submitted = true;
+      this.orderRepository.saveOrder(this.order)
+        .subscribe(() => {
+          this.order.clear();
+          this.submitted = false;
+          this.orderSent = true;
+          },
+          err => alert('Error saving order: ' + err.message)
+        );
+
+    }
+  }
 }
