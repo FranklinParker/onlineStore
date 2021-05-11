361a3b2d2d3d7f5b6d6431fc9ca23d0a35d458dc
FranklinParker <fparker@deliveryman.com>
2021-05-11 15:57:02 -0400

    data

--- data.js
+++ data.js
@@ -2,8 +2,13 @@ module.exports = function () {
   return {
     products: [
       {id: 1, name: 'New Product', category: 'clothes', description: 'Warm', price: 24.50},
-      {id: 2, name: '2nd Product', category: 'clothes', description: 'Jacket', price: 24.50}
+      {id: 2, name: '2nd Product', category: 'clothes', description: 'Jacket', price: 24.50},
+      {id: 3, name: '3rd Product', category: 'clothes', description: 'Sweater', price: 60.50},
+      {id: 4, name: '4th Product', category: 'other', description: 'Bike', price: 100.50}
 
-    ]
+
+
+    ],
+    orders: []
   }
 }
