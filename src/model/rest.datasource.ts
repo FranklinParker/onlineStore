0cc4a1f9839650c121b42b920c5b8e734c3e3f93
FranklinParker <fparker@deliveryman.com>
2021-05-11 09:19:49 -0400

    save

--- src/model/rest.datasource.ts
+++ src/model/rest.datasource.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {ProductModel} from './product.model';
 import {OrderModel} from '../app/models/Order.model';
+import {tap} from 'rxjs/operators';
 
 
 const PORT = 3500;
@@ -10,18 +11,29 @@ const PROTOCAL = 'http';
 
 @Injectable()
 
-export class RestDatasource{
+export class RestDatasource {
   authToken = '';
   baseUrl = '';
+
   constructor(private httpClient: HttpClient) {
     this.baseUrl = `${PROTOCAL}://${location.hostname}:${PORT}/`;
   }
 
-  getProduct(): Observable<ProductModel[]>{
+  getProduct(): Observable<ProductModel[]> {
     return this.httpClient.get<ProductModel[]>(this.baseUrl + 'products');
   }
 
-  saveOrder(order: OrderModel): Observable<OrderModel>{
+  saveOrder(order: OrderModel): Observable<OrderModel> {
     return this.httpClient.post<OrderModel>(this.baseUrl + 'orders', order);
+  }
+
+  authenticate(user: string, password: string): Observable<any> {
+    return this.httpClient.post<any>(this.baseUrl + 'login', {
+      name: user,
+      password
+    })
+      .pipe(
+        tap( resp => this.authToken = resp.success ? resp.token : null)
+      );
   }
 }
