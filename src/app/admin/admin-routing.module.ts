0d97e9b10be2b3b7631e82ffaf6fab357ddaaa7a
FranklinParker <fparker@deliveryman.com>
2021-05-12 09:52:03 -0400

    router outlet

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,17 +3,35 @@ import { RouterModule, Routes } from '@angular/router';
 import {AdminComponent} from './components/admin/admin.component';
 import {AuthComponent} from './components/auth/auth.component';
 import {AuthGuard} from './guard/auth.guard';
+import {ProductListComponent} from './components/product-list/product-list.component';
+import {OrderListComponent} from './components/order-list/order-list.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'main',
+    pathMatch: 'full'
+  },
+  {
+    path: 'main',
     component: AdminComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'products',
+        component: ProductListComponent,
+      },
+      {
+        path: 'orders',
+        component: OrderListComponent,
+      }
+    ],
   },
   {
     path: 'auth',
     component: AuthComponent,
   },
+
 ];
 
 @NgModule({
