b352630ed9001c82025ad585c2635f9d8992f9e7
FranklinParker <fparker@deliveryman.com>
2021-05-13 18:56:45 -0400

    fix

--- src/app/admin/components/order-list/order-list.component.ts
+++ src/app/admin/components/order-list/order-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {OrderRepository} from '../../../../model/order.repository';
+import {OrderModel} from '../../../models/Order.model';
 
 @Component({
   selector: 'app-order-list',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-list.component.scss']
 })
 export class OrderListComponent implements OnInit {
-
-  constructor() { }
+  includeShipped = false;
+  constructor(private orderRepository: OrderRepository) { }
 
   ngOnInit(): void {
   }
+
+  getOrders(): OrderModel[]{
+    return this.orderRepository.getOrders()
+      .filter(o => this.includeShipped || !o.shipped);
+  }
+
+  delete(id: number): void{
+    this.orderRepository.deleteOrder(id);
+  }
+
+  markedAsShipped(order: OrderModel): void{
+    order.shipped = true;
+    this.orderRepository.saveOrder(order);
+  }
+
 
 }
